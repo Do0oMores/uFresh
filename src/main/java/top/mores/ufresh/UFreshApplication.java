2e8967966033d57bf2fdc2db98fddc42c9d5e22f
Do0oMores <87219087+Do0oMores@users.noreply.github.com>
2024-09-27 16:11:51 +0800

    init

---
+++ src/main/java/top/mores/ufresh/UFreshApplication.java
@@ -0,0 +1,13 @@
+package top.mores.ufresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UFreshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UFreshApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/mores/ufresh/UFreshApplicationTests.java
@@ -0,0 +1,13 @@
+package top.mores.ufresh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UFreshApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
