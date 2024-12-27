99da36fc7a114001d53c7ff59bcebfc468c90846
Do0oMores <87219087+Do0oMores@users.noreply.github.com>
2024-12-27 21:42:17 +0800

    :card_file_box: 合并购物车表（cart）到购物车物品表（cart_item）

--- src/main/java/top/mores/ufresh/POJO/Cart_items.java
+++ src/main/java/top/mores/ufresh/POJO/Cart_items.java
@@ -1,11 +1,31 @@
 package top.mores.ufresh.POJO;
 
+import java.time.LocalDateTime;
+
 public class Cart_items {
     private int cart_item_id;
-    private int cart_id;
+    private int user_id;
     private int commodity_id;
     private int amount;
     private double added_price;
+    private int spec_id;
+    private LocalDateTime added_time;
+
+    public int getSpec_id() {
+        return spec_id;
+    }
+
+    public void setSpec_id(int spec_id) {
+        this.spec_id = spec_id;
+    }
+
+    public LocalDateTime getAdded_time() {
+        return added_time;
+    }
+
+    public void setAdded_time(LocalDateTime added_time) {
+        this.added_time = added_time;
+    }
 
     public int getCart_item_id() {
         return cart_item_id;
@@ -15,12 +35,12 @@ public class Cart_items {
         this.cart_item_id = cart_item_id;
     }
 
-    public int getCart_id() {
-        return cart_id;
+    public int getUser_id() {
+        return user_id;
     }
 
-    public void setCart_id(int cart_id) {
-        this.cart_id = cart_id;
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
     }
 
     public int getCommodity_id() {
@@ -51,10 +71,12 @@ public class Cart_items {
     public String toString() {
         return "Cart_items{" +
                 "cart_item_id=" + cart_item_id +
-                ", cart_id=" + cart_id +
+                ", user_id=" + user_id +
                 ", commodity_id=" + commodity_id +
                 ", amount=" + amount +
                 ", added_price=" + added_price +
+                ", spec_id=" + spec_id +
+                ", added_time=" + added_time +
                 '}';
     }
 }
